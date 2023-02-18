f79956e9afb71b1e4b3605d501dca0d61bbc6323
Iury <iurygfranca@gmail.com>
2023-02-17 20:34:01 -0400

    feat: initial commit ✨

---
+++ src/App.vue
@@ -0,0 +1,5 @@
+<template>
+  <RouterView
+    class="container min-h-screen bg-white font-sans text-slate-900 antialiased dark:bg-slate-900 dark:text-slate-50"
+  />
+</template>
---
+++ tailwind.config.js
@@ -0,0 +1,48 @@
+/* eslint-disable no-undef */
+
+const { fontFamily } = require('tailwindcss/defaultTheme')
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,vue}'],
+  theme: {
+    container: {
+      center: true,
+      padding: '2.5rem',
+      screens: {
+        '2xl': 'max-content',
+      },
+    },
+    screens: {
+      xs: '430px',
+      sm: '580px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1280px',
+      '2xl': '1536px',
+    },
+    extend: {
+      fontFamily: {
+        sans: ['Inter var, sans-serif', ...fontFamily.sans],
+      },
+      keyframes: {
+        'accordion-down': {
+          from: { height: 0 },
+          to: { height: 'var(--radix-accordion-content-height)' },
+        },
+        'accordion-up': {
+          from: { height: 'var(--radix-accordion-content-height)' },
+          to: { height: 0 },
+        },
+      },
+      animation: {
+        'accordion-down': 'accordion-down 0.2s ease-out',
+        'accordion-up': 'accordion-up 0.2s ease-out',
+      },
+    },
+  },
+  plugins: [
+    require('tailwindcss-animate'),
+    require('prettier-plugin-tailwindcss'),
+  ],
+}
